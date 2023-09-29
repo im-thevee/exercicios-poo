49d88c8560de43fadca063ecc3e0b0e2599f8401
im-thevee <145984104+im-thevee@users.noreply.github.com>
2023-09-29 17:53:20 -0300

    segundo commit

--- estruturaCondicional/condicionalEx4.py
+++ estruturaCondicional/condicionalEx4.py
@@ -1,29 +1,38 @@
 """
-■ Escreva um programa que sempre escolhe o menor caminho a ser percorrido pelo usuário em
+- Escreva um programa que sempre escolhe o menor caminho a ser percorrido pelo usuário em
 função do local onde se encontra e as opções de caminho a serem seguidas. O usuário sempre
 parte do ponto A. O usuário deverá fornecer os valores de distância entre os pontos e o programa
 deverá apresentar o caminho a ser percorrido e a distância, conforme o exemplo. Sua solução
 deve utilizar apenas estruturas condicionais.
 """
-escolhas = ["A"]
 b = int(input("Digite a distancia de B: "))
 c = int(input("Digite a distancia de C: "))
+caminho_percorrido = "A"
+distancia_percorrida = 0
 
 if ( b < c ):
-    escolhas.append(f"B = {b}")
+    caminho_percorrido += " → B"
+    distancia_percorrida += b
     d = int(input("Digite a distancia de D: "))
     e = int(input("Digite a distancia de E: "))
     if ( d < e ):
-        escolhas.append(f"D = {d}")
+        caminho_percorrido += " → D"
+        distancia_percorrida += d
+    
     elif ( e < d ):
-        escolhas.append(f"E = {e}") 
-         
+        caminho_percorrido += " → E" 
+        distancia_percorrida += e
+
 if ( c < b ):
-    escolhas.append(f"C = {c}")
+    caminho_percorrido += " → C"
+    distancia_percorrida += c
     f = int(input("Digite a distancia de F: "))
     g = int(input("Digite a distancia de G: "))
     if ( f < g ):
-        escolhas.append(f"F = {f}")
+        caminho_percorrido += " → F"
+        distancia_percorrida += f
     elif ( g < f ):
-        escolhas.append(f"G = {g}") 
-print(escolhas)
+        caminho_percorrido += " → G" 
+        distancia_percorrida += g
+print(f"Caminho percorrido: {caminho_percorrido}")
+print(f"Distância percorrida: {distancia_percorrida}")
